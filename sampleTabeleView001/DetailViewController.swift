71a16fd8ecda807cdffcff9f35fef160e7da2ddf
小林由知 <kobayashiyoshitomo@USER-no-MacBook-Pro.local>
2017-11-04 01:39:09 +0800

    moemoe_fix

---
+++ sampleTabeleView001/DetailViewController.swift
@@ -0,0 +1,85 @@
+//
+//  DetailViewController.swift
+//  sampleTabeleView001
+//
+//  Created by 小林由知 on 2017/11/03.
+//  Copyright © 2017年 Yoshitomo Kobatashi. All rights reserved.
+//
+
+import UIKit
+
+
+class DetailViewController: UIViewController {
+    
+    var teaList = [
+        "ダージリン"
+        , "アッサム"
+        , "オレンジペコ"
+        , "ねこにゃー"
+    ]
+    
+    var descriptionList = [
+        "CV：喜多村英梨\n学年：3年\n担当：隊長・戦車長\n身長：158cm\n出身地：神奈川県横浜市\n現住所：聖グロリアーナ女学院女子寮\n家族構成：父、母、妹\n血液型：AB型\n誕生日：9月17日\n年齢：17歳\n好きな食べ物：ミートパイ\n好きなお菓子：カスタードプリン\n好きな飲み物：ダージリンティー\n好きな花：青いバラ（奇跡）\n好きな戦車：センチュリオン\n得意科目：英語\n趣味：紅茶\n日課：名言集を読む\n座右の銘：行いの美しいものは、姿も麗しい"
+        , "CV：明坂聡美\n学年：3年\n担当：砲手\n身長：150cm\n出身：神奈川県横浜市\n 現住所：聖グロリアーナ女学院女子寮\n家族構成：父、母、兄\n血液型：０型\n誕生日：12月8日\n年齢：17歳\n好きな食べ物：ローストビーフ\n好きなお菓子：ジンジャーナッツ\n好きな飲み物：アッサム\n好きな花：ミニバラ（無意識の美）\n好きな戦車：チャレンジャー\n得意科目：数学\n苦手科目：漢文\n趣味：紅茶\n日課：ジョギング\n 座右の銘：道理は全てを支配する"
+        , "CV：石原舞\n学年：1年\n担当：装填手兼通信手\n身長：144cm\n出身地：神奈川県横浜市\n現住所：聖グロリアーナ女学院女子寮\n家族構成：父、母\n血液型：０型\n誕生日：7月10日\n年齢：16歳\n好きな食べ物：ホットクロスパン\n好きなお菓子：トライフル\n好きな飲み物：オレンジペコ\n好きな花：オレンジバラ（無邪気）\n好きな戦車：クルセイダ\n得意科目：英語\n苦手科目：数学\n趣味：紅茶\n日課：詩集を読む\n座右の銘：満足しない者は、何も持っていないのと同じ\n備考：操縦もうまく、また資料整理能力の高さから、ダージリンの補佐として抜擢"
+        , "所属校：県立大洗女子学園\n学年：2年生\n所属チーム：アリクイさんチーム\n担当：チームリーダー・戦車長・通信手\n身長：168㎝\n出身地：茨城県鉾田市\n血液型：B型\n好きな食べ物：ポテトチップ\n好きな花：バラ\n好きな戦車：レオパルト2\nCV：葉山いくみ\n \nスラーダーを動かすとねこにゃーの素顔が見れるよ！\n　※画像のズレが解消できません。（画像サイズは同じ。myImageViweの前後のサイズ差を幅4pt、高さ4ptに設定済み。）※"
+    ]
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var myImageViwe: UIImageView!
+    @IBOutlet weak var myImageViweBack: UIImageView!
+    
+    @IBOutlet weak var myTextView: UITextView!
+    @IBAction func mySlider(_ sender: UISlider) {
+        myImageViwe.alpha = CGFloat(sender.value)
+    }
+    @IBOutlet weak var mySlider: UISlider!
+    
+    // 受け取った行番号を保存しておく変数（箱）
+    var passedIndex:Int = -1
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("２枚目に表示されたよ。うふふ。")
+        print("受け取った行番号\(passedIndex)")
+
+        myLabel.text = teaList[passedIndex]
+        myImageViwe.image = UIImage(named:"sample\(passedIndex).jpeg")
+        
+        if passedIndex == 3 {
+            myImageViweBack.image = UIImage(named:"sample3_back.jpeg")
+        }else{
+            mySlider.isHidden = true
+        }
+
+        myTextView.text = descriptionList[passedIndex]
+        myTextView.sizeToFit() // 文字数に合わせて縦に伸びます。
+
+//        if passedIndex != 3 {
+//            mySlider.isHidden = true
+//        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+//    myImageViwe.alpha = "\(sender.value)"
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- sampleTabeleView001/ViewController.swift
+++ sampleTabeleView001/ViewController.swift
@@ -15,12 +15,20 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     @IBOutlet weak var myTableView: UITableView!
     
     //表示したいデータ（配列）
-    var teaList = ["ダージリン", "アールグレイ", "アッサム", "オレンジペコ", "ああああああああああああああああああああああああああああああああああああああああああああああおかゆさんがああああああああ"]
+    var teaList = [
+          "ダージリン"
+        , "アッサム"
+        , "オレンジペコ"
+        , "ねこにゃー"
+    ]
     
+    
+    // 何行目が選択されてら保存する変数
+    // -1は、行番号が何も選択されたいない。という目印になる。
+    var selectedIndex = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     //2. 行数の表示（numberOfRowsInSectionを検索）
@@ -36,16 +44,50 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         
         //表示したい文字の設定（セルの中には文字、画像も入る）
         cell.textLabel?.text = teaList[indexPath.row]
-        cell.textLabel?.font = UIFont(name: "AmericanTypewriter", size: 50)
-//文字数が多い時の対応方法はWhat!?        //cell.textLabel?.sizeToFit() // 文字数に合わせて縦に伸びます。
-        cell.textLabel?.textColor = UIColor.blue
+        cell.textLabel?.font = UIFont(name: "AmericanTypewriter", size: 16)
+        cell.textLabel?.lineBreakMode = .byCharWrapping
+        //byCharWrappingを変えれば、折り返しとかも可能。
+        
+        //cell.textLabel?.sizeToFit() // 文字数に合わせて縦に伸びます。
+        cell.textLabel?.textColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        
+        if indexPath.row % 2 == 0{
+            cell.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+        }else if indexPath.row % 2 == 1{
+            cell.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        }
         //cell.textLabel?.textAlignment = .center // text内　横軸中央揃え
+        
+        tableView.separatorColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
 
         //文字を設定したセルを返す
         return cell
     }
     
+    //セルがタップされたら発動
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.row)行目をタップしました。")
         
+        // 選択された行番号を保存
+        selectedIndex = indexPath.row
+        
+        //セグエの名前を指定して、画面　処理を発動
+        performSegue(withIdentifier: "showDetail", sender: nil)
+        
+    }
+
+    //セグエを使って、画面遷移をするときに発動
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // 次の画面をインスタンス（オブジェクト）を取得
+        //as! DetailViewControllerがダウンキャスト変換している箇所
+        var dvc:DetailViewController = segue.destination as! DetailViewController
+        
+        // 次の画面をプロパティ（メンバ変数）passedIndexに選択された行番号を渡す。
+        dvc.passedIndex = selectedIndex
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
